9f9d99f23608f1c98f1eed605a49c99871535643
Tetleysteabags <79264412+Tetleysteabags@users.noreply.github.com>
2022-02-09 13:35:01 +0200

    Update Convert CSV to JSON.py

--- Convert CSV to JSON.py
+++ Convert CSV to JSON.py
@@ -6,8 +6,8 @@ Created on Wed Feb  9 13:09:20 2022
 """
 import pandas as pd
 #Import the CSV file you want to convert
-df = pd.read_csv (r'C:\Users\Thoma\Desktop\Coding\YPO Data Connect CSV.csv',encoding="mbcs")
+df = pd.read_csv (r'Location of file\filename.csv',encoding="mbcs")
 #check the dataset has imported correctly
 df.head()
 #conver the imported file to JSON and save in the desired location with appropriate name
-df.to_json(r'C:\Users\Thoma\Desktop\Coding\Converted YPO Data Connect CSV.json')
+df.to_json(r'target location to save file\desired file name.json')
